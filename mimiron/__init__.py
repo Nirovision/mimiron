6de12c30bf9461eb058a275103eabc803a34f83c
David Vuong <david.vuong256@gmail.com>
2017-02-09 19:10:01 +1100

    chore(pkg): bump 0.1.1

--- mimiron/__init__.py
+++ mimiron/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 
-__version_info__ = (0, 1, 0)
+__version_info__ = (0, 1, 1)
 __version__ = '.'.join([unicode(i) for i in __version_info__])
 
 __author__ = 'David Vuong'
