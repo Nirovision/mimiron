9d1e16d68d4212819587e5fcdfb303b688275322
David Vuong <david.vuong256@gmail.com>
2017-03-09 00:21:57 +1100

    test(dockerhub): add test_build_image_abspath test

---
+++ test/vendor/test_dockerhub.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from collections import namedtuple
+import pytest
+
+from mimiron.vendor import dockerhub
+
+
+class TestDockerHub(object):
+    def setup_method(self):
+        DockerAuthMock = namedtuple('DockerAuthMock', ['username', 'password', 'org'], verbose=True)
+        self.auth = DockerAuthMock('example-username', 'example-password', 'imageintelligence')
+
+    def test_build_image_abspath_normal_case(self):
+        image_name = 'example-image'
+        image_tag = '123123123'
+
+        expected_output = '%s/%s:%s' % (self.auth.org, image_name, image_tag)
+        result = dockerhub.build_image_abspath(self.auth, image_name, image_tag)
+
+        assert result == expected_output
+
