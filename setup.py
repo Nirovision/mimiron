3a107af4d6dbc0db29d33f516af0a14ea3c0c40f
David Vuong <david.vuong256@gmail.com>
2017-09-29 15:24:50 +1000

    chore(requirements): re-structured req and setup.py
    This also adds jsonschema as a dependency. This chnage will make it so
    that requirements.txt only contains development specific dependencies.
    All necessary dependencies will be specified inside setup.py

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     install_requires=[
         'docopt==0.6.2',
         'terminaltables==3.1.0',
+        'jsonschema==2.6.0',
         'GitPython==2.1.1',
         'requests==2.13.0',
         'humanize==0.5.1',
