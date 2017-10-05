803121b38161beaf09d3fc2d08dfdb45095f5bc0
David Vuong <david.vuong256@gmail.com>
2017-10-05 11:14:37 +1100

    feat+fix(status): status now works with new TFVarsConfig
    As a result, it can display the status of multiple repos

--- mimiron/core/commands/status.py
+++ mimiron/core/commands/status.py
@@ -2,16 +2,10 @@
 from . import Command as _Command
 from .. import io
 
-from ...vendor.terraform import TFVarsConfig
 from ...vendor.git_extensions import extensions as git_ext
 
 
 class Status(_Command):
-    def validate_and_configure(self):
-        super(self.__class__, self).validate_and_configure()
-
-        self.tf = TFVarsConfig(self.tfvars_path)
-
     def _format_row(self, artifact):
         image_tag = artifact['image'].split(':')[-1]
         desired_count = int(artifact.get('desired_count', 0))
@@ -22,20 +16,26 @@ class Status(_Command):
         memory = artifact.get('memory', io.add_color('n/a', 'red'))
         return [image_tag, desired_count, cpu, memory]
 
-    def run(self):
-        git_ext.sync_updates(self.deployment_repo)
-        self.tf.load()  # sync_updates may have changed tfvars
-
-        services = self.tf.get_services()
-
-        docker_org = self.config['DOCKER_ORG']
+    def _build_status_table(self, deployment_repo):
+        services = deployment_repo['tfvars'].get_services()
+        docker_org = self.config.get('dockerhub')['organization']
         table_data = [
             ['id', 'name', 'image tag (%s)' % docker_org, 'desired count', 'cpu units', 'memory (mb)'],
         ]
         for i, service_name in enumerate(sorted(services.iterkeys()), 1):
             artifact = services[service_name]
             table_data.append([i, service_name] + self._format_row(artifact))
+        return table_data
 
-        io.info('displaying "%s" active&inactive services on %s' % (docker_org, self.env))
-        io.print_table(table_data, 'current %s artifacts' % self.env)
+    def run(self):
+        io.info('performing git sync across all specified repositories...')
+        for deployment_repo in self.config.get('terraformRepositories'):
+            git_ext.sync_updates(deployment_repo['git'])
+            deployment_repo['tfvars'].load()  # sync_updates may have changed tfvars.
+
+            table_data = self._build_status_table(deployment_repo)
+            env = self.kwargs['env'] or deployment_repo['defaultEnvironment']
+            docker_org = self.config.get('dockerhub')['organization']
+            io.info('displaying "%s" active&inactive services on "%s"' % (docker_org, env))
+            io.print_table(table_data, 'current %s artifacts' % env)
         io.warn('only dockerized services are shown here (i.e. no lambda)')
