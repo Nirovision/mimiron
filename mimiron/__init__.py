eb1dd4ef75ba5d30d34ad3ba596a319215590626
David Vuong <david.vuong256@gmail.com>
2018-01-18 15:22:45 +1100

    chore(__init__): define __all__

--- mimiron/__init__.py
+++ mimiron/__init__.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+__all__ = [
+    '__version_info__',
+    '__version__',
+    '__author__',
+    '__author_email__',
+]
 
 __version_info__ = (0, 4, 2)
 __version__ = '.'.join([unicode(i) for i in __version_info__])
